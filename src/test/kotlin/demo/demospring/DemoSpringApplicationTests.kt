5aff5115d21f88f0446e757cc5088bda9baabf84
Jina <wonjieunkk@gmail.com>
2023-07-07 22:16:04 +0900

    Create a Spring Boot project

---
+++ src/test/kotlin/demo/demospring/DemoSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package demo.demospring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoSpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
