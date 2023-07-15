d7e7db79fb7a584006f406f6b09e3255f4a2c30e
Jina <wonjieunkk@gmail.com>
2023-07-15 22:55:57 +0900

    Update BasicController.kt

--- src/main/kotlin/demo/demospring/controller/BasicController.kt
+++ src/main/kotlin/demo/demospring/controller/BasicController.kt
@@ -4,12 +4,14 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 class BasicController {
     @GetMapping("demo")
     fun helloMvc(model: Model): String {
-        model.addAttribute("name", "Jina")
+        val name = "Jina"
+        model.addAttribute("name", name)
         return "basic-template"
     }
 
@@ -17,5 +19,23 @@ class BasicController {
     fun demoMvc(@RequestParam("name") name: String, model: Model): String {
         model.addAttribute("name", name)
         return "basic-template"
+    }
+
+    @GetMapping("demo-string")
+    @ResponseBody
+    fun demoString(@RequestParam("name") name: String): String {
+        return "hello $name"
+    }
+
+    @GetMapping("demo-api")
+    @ResponseBody
+    fun demoApi(@RequestParam("name") name: String): Demo {
+        val demo = Demo()
+        demo.name = name
+        return demo
+    }
+
+    class Demo {
+        var name = ""
     }
 }
